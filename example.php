a292317a604e5e06c9adc03be75ba0c03c456723
Mike van Riel <me@mikevanriel.com>
2014-11-11 06:55:08 +0100

    Expanded example

--- example.php
+++ example.php
@@ -1,20 +1,44 @@
 <?php
+/**
+ * Create a project map (similar to a sitemap) of project.
+ *
+ * The Reflection component is capable of analyzing one or more files into a hierarchy of objects representing the
+ * structure of your project. It does this by analyzing the source code of each individual file using the
+ * `analyze()` method in the Analyzer class.
+ *
+ * Because the Analyzer class requires a whole series of objects that interact together a factory method `create()`
+ * is available. This method instantiates all objects and provides a reasonable default to start using the Analyzer.
+ *
+ * There is also a Service Provider (`\phpDocumentor\Descriptor\ServiceProvider`) that can be used with either Silex
+ * or Cilex instead of using the factory method; this will make it easier to plug in your own features if you want to.
+ */
 
-// use Composer's autoloader to load all classes
+// use Composer's autoloader to allow the application to automatically load all classes on request.
 include 'vendor/autoload.php';
 
-use phpDocumentor\Descriptor\ProjectDescriptorBuilder;
+use phpDocumentor\Descriptor\Analyzer;
 
-// Load a file's contents using the SplFileObject class
+// Create a new Analyzer with which we can analyze a PHP source file
+$analyzer = Analyzer::create();
+
+// Load a file that is to be analyzed
 $splFileObject = new \SplFileObject('tests/example.file.php');
 
-// Create a new object that constructs a Project for us
-$builder = ProjectDescriptorBuilder::create();
+// Analyze the given file, this will return a the structure of a single file as a
+// `\phpDocumentor\Descriptor\FileDescriptor` class and populate a project descriptor object in the Analyzer.
+$analyzer->analyze($splFileObject);
 
-// Feed the SplFileObject containing the contents of the file to
-// the builder
-$builder->buildFileUsingSourceData($splFileObject);
+// The analyzer collects all data from the analyzed files into a Project that can be retrieved using the
+// `getProjectDescriptor()` method.
+//
+// The returned Project object is of class `phpDocumentor\Descriptor\ProjectDescriptor`, see its DocBlock for more
+// information on it.
+$project = $analyzer->getProjectDescriptor();
 
-// Fetch the constructed project
-$project = $builder->getProjectDescriptor();
+// As an example of what you can do, let's list all class names in the file 'tests/example.file.php'.
+echo 'List all classes in the example source file: ' . PHP_EOL;
 
+/** @var \phpDocumentor\Descriptor\ClassDescriptor $class */
+foreach ($project->getFiles()->get('tests/example.file.php')->getClasses() as $class) {
+    echo '- ' . $class->getFullyQualifiedStructuralElementName() . PHP_EOL;
+}
