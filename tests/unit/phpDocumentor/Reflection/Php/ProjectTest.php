437df4458c4bb9c0ae7e7dcd6e5097e54f5251bc
Jaapio <jaap@ijaap.nl>
2015-06-22 22:50:06 +0200

    fix copyright

--- tests/unit/phpDocumentor/Reflection/Php/ProjectTest.php
+++ tests/unit/phpDocumentor/Reflection/Php/ProjectTest.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * phpDocumentor
+ * This file is part of phpDocumentor.
  *
- * PHP Version 5.3
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  *
- * @copyright 2010-2013 Mike van Riel / Naenius (http://www.naenius.com)
+ * @copyright 2010-2015 Mike van Riel<mike@phpdoc.org>
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      http://phpdoc.org
  */
 
 namespace phpDocumentor\Reflection\Php;
 
-use \Settings;
 use phpDocumentor\Reflection\Fqsen;
 
 /**
