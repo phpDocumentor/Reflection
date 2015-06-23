c0361abe77c984bdd04e252ed0a0fcf4cf24f53b
Jaapio <jaap@ijaap.nl>
2015-06-23 21:40:04 +0200

    add interface strategy

---
+++ src/phpDocumentor/Reflection/Php/Factory/Interface_.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2010-2015 Mike van Riel<mike@phpdoc.org>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+
+namespace phpDocumentor\Reflection\Php\Factory;
+use InvalidArgumentException;
+use phpDocumentor\Reflection\Element;
+use phpDocumentor\Reflection\Php\ProjectFactoryStrategy;
+use phpDocumentor\Reflection\Php\StrategyContainer;
+use PhpParser\Comment\Doc;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassConst;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Interface_ as InterfaceNode;
+use phpDocumentor\Reflection\Php\Interface_ as InterfaceElement;
+
+/**
+ * Strategy to create a InterfaceElement including all sub elements.
+ */
+final class Interface_ implements ProjectFactoryStrategy
+{
+
+    /**
+     * Returns true when the strategy is able to handle the object.
+     *
+     * @param InterfaceNode $object object to check.
+     * @return boolean
+     */
+    public function matches($object)
+    {
+        return $object instanceof InterfaceNode;
+    }
+
+    /**
+     * Creates an Interface_ out of the given object.
+     * Since an object might contain other objects that need to be converted the $factory is passed so it can be
+     * used to create nested Elements.
+     *
+     * @param InterfaceNode $object object to convert to an Element
+     * @param StrategyContainer $strategies used to convert nested objects.
+     * @return InterfaceElement
+     *
+     * @throws InvalidArgumentException when this strategy is not able to handle $object
+     */
+    public function create($object, StrategyContainer $strategies)
+    {
+        if (!$this->matches($object)) {
+            throw new InvalidArgumentException(
+                sprintf('%s cannot handle objects with the type %s',
+                    __CLASS__,
+                    is_object($object) ? get_class($object) : gettype($object)
+                )
+            );
+        }
+
+        $docBlock = $this->createDocBlock($object->getDocComment(), $strategies);
+
+        $interface = new InterfaceElement($object->fqsen, $docBlock);
+
+        if (isset($object->stmts)) {
+            foreach ($object->stmts as $stmt) {
+                switch (get_class($stmt)) {
+                    case ClassMethod::class:
+                        $method = $this->createMember($stmt, $strategies);
+                        $interface->addMethod($method);
+                        break;
+                    case ClassConst::class:
+                        $constants = new ClassConstantIterator($stmt);
+                        foreach ($constants as $const) {
+                            $element = $this->createMember($const, $strategies);
+                            $interface->addConstant($element);
+                        }
+                        break;
+                }
+            }
+        }
+
+        return $interface;
+    }
+
+    /**
+     * @param Node|ClassConstantIterator $stmt
+     * @param StrategyContainer $strategies
+     *
+     * @return Element
+     */
+    private function createMember($stmt, StrategyContainer $strategies)
+    {
+        $strategy = $strategies->findMatching($stmt);
+        return $strategy->create($stmt, $strategies);
+    }
+
+    /**
+     * @param Doc $docBlock
+     * @param StrategyContainer $strategies
+     * @return null|\phpDocumentor\Reflection\DocBlock
+     */
+    private function createDocBlock(Doc $docBlock = null, StrategyContainer $strategies)
+    {
+        if ($docBlock === null) {
+            return null;
+        }
+
+        $strategy = $strategies->findMatching($docBlock);
+        return $strategy->create($docBlock, $strategies);
+    }
+}
---
+++ tests/unit/phpDocumentor/Reflection/Php/Factory/Interface_Test.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2010-2015 Mike van Riel<mike@phpdoc.org>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+
+namespace phpDocumentor\Reflection\Php\Factory;
+
+use Mockery as m;
+use phpDocumentor\Reflection\Fqsen;
+use phpDocumentor\Reflection\Php\StrategyContainer;
+use PhpParser\Comment\Doc;
+use PhpParser\Node\Const_;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\ClassConst;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Interface_ as InterfaceNode;
+use phpDocumentor\Reflection\DocBlock as DocBlockElement;
+use phpDocumentor\Reflection\Php\Constant as ConstantElement;
+use phpDocumentor\Reflection\Php\Interface_ as InterfaceElement;
+use phpDocumentor\Reflection\Php\Method as MethodElement;
+
+/**
+ * Test case for Interface_
+ * @coversDefaultClass phpDocumentor\Reflection\Php\Factory\Interface_
+ * @covers ::<private>
+ */
+class Interface_Test extends TestCase
+{
+    protected function setUp()
+    {
+        $this->fixture = new Interface_();
+    }
+
+    /**
+     * @covers ::matches
+     */
+    public function testMatches()
+    {
+        $this->assertFalse($this->fixture->matches(new \stdClass()));
+        $this->assertTrue($this->fixture->matches(m::mock(InterfaceNode::class)));
+    }
+
+    /**
+     * @covers ::create
+     */
+    public function testSimpleCreate()
+    {
+        $strategiesMock = m::mock(StrategyContainer::class);
+        $interfaceMock = $this->buildClassMock();
+        $interfaceMock->shouldReceive('getDocComment')->andReturnNull();
+
+        /** @var InterfaceElement $class */
+        $class = $this->fixture->create($interfaceMock, $strategiesMock);
+
+        $this->assertInstanceOf(InterfaceElement::class, $class);
+        $this->assertEquals('\Space\MyInterface', (string)$class->getFqsen());
+    }
+
+    /**
+     * @covers ::create
+     */
+    public function testCreateWithDocBlock()
+    {
+        $doc = m::mock(Doc::class);
+        $interfaceMock = $this->buildClassMock();
+        $interfaceMock->shouldReceive('getDocComment')->andReturn($doc);
+
+        $docBlock = new DocBlockElement('');
+
+        $containerMock = m::mock(StrategyContainer::class);
+        $containerMock->shouldReceive('findMatching->create')
+            ->once()
+            ->with($doc, $containerMock)
+            ->andReturn($docBlock);
+
+        /** @var InterfaceElement $interface */
+        $interface = $this->fixture->create($interfaceMock, $containerMock);
+
+        $this->assertSame($docBlock, $interface->getDocBlock());
+    }
+
+    /**
+     * @covers ::create
+     */
+    public function testWithMethodMembers()
+    {
+        $method1 = new ClassMethod('\Space\MyInterface::method1');
+        $method1Descriptor = new MethodElement(new Fqsen('\Space\MyInterface::method1'));
+        $strategiesMock = m::mock(StrategyContainer::class);
+        $interfaceMock = $this->buildClassMock();
+        $interfaceMock->shouldReceive('getDocComment')->andReturnNull();
+        $interfaceMock->stmts = [
+            $method1
+        ];
+
+        $strategiesMock->shouldReceive('findMatching->create')
+            ->with($method1, $strategiesMock)
+            ->andReturn($method1Descriptor);
+
+        $this->fixture->create($interfaceMock, $strategiesMock);
+
+        /** @var InterfaceElement $interface */
+        $interface = $this->fixture->create($interfaceMock, $strategiesMock);
+
+        $this->assertInstanceOf(InterfaceElement::class, $interface);
+        $this->assertEquals('\Space\MyInterface', (string)$interface->getFqsen());
+        $this->assertEquals(
+            ['\Space\MyInterface::method1' => $method1Descriptor],
+            $interface->getMethods()
+        );
+    }
+
+    /**
+     * @covers ::create
+     */
+    public function testWithConstants()
+    {
+        $const = new Const_('\Space\MyClass::MY_CONST', new Variable('a'));
+        $constant = new ClassConst([$const]);
+
+        $result = new ConstantElement(new Fqsen('\Space\MyClass::MY_CONST'));
+        $strategiesMock = m::mock(StrategyContainer::class);
+        $strategiesMock->shouldReceive('findMatching->create')
+            ->with(m::type(ClassConstantIterator::class), $strategiesMock)
+            ->andReturn($result);
+        $classMock = $this->buildClassMock();
+        $classMock->shouldReceive('getDocComment')->andReturnNull();
+        $classMock->stmts = [
+            $constant
+        ];
+
+        /** @var ClassElement $class */
+        $class = $this->fixture->create($classMock, $strategiesMock);
+
+        $this->assertEquals(
+            [
+                '\Space\MyClass::MY_CONST' => $result,
+            ],
+            $class->getConstants()
+        );
+    }
+
+    /**
+     * @return m\MockInterface|InterfaceNode
+     */
+    private function buildClassMock()
+    {
+        $interfaceMock = m::mock(InterfaceNode::class);
+        $interfaceMock->fqsen = new Fqsen('\Space\MyInterface');
+        return $interfaceMock;
+    }
+}
