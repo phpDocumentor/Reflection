a499141c94cc1738a9b7c677f74f3761b52f86c5
Chuck Burgess <ashnazg@php.net>
2018-02-27 13:14:28 -0600

    use PREFER rather than ONLY;

--- src/phpDocumentor/Reflection/Php/NodesFactory.php
+++ src/phpDocumentor/Reflection/Php/NodesFactory.php
@@ -59,7 +59,7 @@ class NodesFactory
      *  ParserFactory::PREFER_PHP5, ParserFactory::ONLY_PHP7 or ParserFactory::ONLY_PHP5
      * @return static
      */
-    public static function createInstance($kind = ParserFactory::ONLY_PHP7): self
+    public static function createInstance($kind = ParserFactory::PREFER_PHP7): self
     {
         $parser = (new ParserFactory())->create($kind);
         $traverser = new NodeTraverser(false);
