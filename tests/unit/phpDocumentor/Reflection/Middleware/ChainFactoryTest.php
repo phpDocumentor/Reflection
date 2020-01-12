baffaf2aa040297d3ea4c902c503e75bd79be87b
Mike van Riel <mike@ingewikkeld.net>
2020-01-12 07:48:42 +0100

    Add test for ChainFactory

---
+++ tests/unit/phpDocumentor/Reflection/Middleware/ChainFactoryTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link http://phpdoc.org
+ */
+
+namespace phpDocumentor\Reflection\Middleware;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+/**
+ * @coversDefaultClass \phpDocumentor\Reflection\Middleware\ChainFactory
+ * @covers ::<private>
+ */
+final class ChainFactoryTest extends TestCase
+{
+    /**
+     * @covers ::createExecutionChain
+     */
+    public function testItCreatesAChainOfCallablesThatWillInvokeAllMiddlewares()
+    {
+        $exampleCommand = new class implements Command {
+        };
+
+        $middleware1 = $this->givenAMiddleware('c');
+        $middleware2 = $this->givenAMiddleware('b');
+
+        $chain = ChainFactory::createExecutionChain(
+            [$middleware1, $middleware2],
+            function () {
+                $result = new stdClass();
+                $result->counter = 'a';
+                return $result;
+            }
+        );
+
+        $this->assertInstanceOf(stdClass::class, $chain(new $exampleCommand));
+        $this->assertSame('abc', $chain(new $exampleCommand)->counter);
+    }
+
+    /**
+     * @covers ::createExecutionChain
+     */
+    public function testItThrowsAnExceptionIfAnythingOtherThanAMiddlewareIsPassed()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Middleware must be an instance of phpDocumentor\Reflection\Middleware\Middleware but string was given'
+        );
+        $middleware = '1';
+
+        ChainFactory::createExecutionChain(
+            [$middleware],
+            function () {
+                return new stdClass();
+            }
+        );
+    }
+
+    private function givenAMiddleware(string $exampleValue) : Middleware
+    {
+        return new class($exampleValue) implements Middleware {
+            private $exampleAddedValue;
+
+            public function __construct(string $exampleAddedValue)
+            {
+                $this->exampleAddedValue = $exampleAddedValue;
+            }
+
+            public function execute(Command $command, callable $next) : object
+            {
+                $result = $next($command);
+                $result->counter .= $this->exampleAddedValue;
+                return $result;
+            }
+        };
+    }
+}
