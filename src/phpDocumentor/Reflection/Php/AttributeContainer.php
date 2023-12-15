a3450fef1264f90cf95c1fa89ff99d5913a44fcb
Jaapio <jaap@ijaap.nl>
2023-12-15 13:54:01 +0100

    Add getAttributes to container

--- src/phpDocumentor/Reflection/Php/AttributeContainer.php
+++ src/phpDocumentor/Reflection/Php/AttributeContainer.php
@@ -7,4 +7,7 @@ namespace phpDocumentor\Reflection\Php;
 interface AttributeContainer
 {
     public function addAttribute(Attribute $attribute): void;
+
+    /** @return Attribute[] */
+    public function getAttributes(): array;
 }
